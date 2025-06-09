0f49b4dcaf2e5df22f809bbf7fdf21cdf8f844fe
Sira Pornsiriprasert <code@psira.me>
2025-06-09 22:03:53 +0700

    Auto saved by Logseq

--- .github/scripts/inject_html.sh
+++ .github/scripts/inject_html.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-metas='<meta property="og:title" content="cyber docs">
-
+meta='<meta property="og:title" content="cyber docs">
 <meta name="description" content="cyber docs">
 <meta property="og:description" content="cyber docs">
+<meta property="og:image" content="/static/img/SVLMD-logo.svg">'
 
-<meta property="og:image" content="/static/img/logo.png">'
+script='<script>
+  document.title = document.title + " | SVLMD";
+</script>'
 
 file="$GITHUB_WORKSPACE/build/index.html"
 
-sed -i "/<\/head>/i\\
-$(echo "$metas" | sed 's/$/\\n/' | tr -d '\n')\\
-" "$file"
+injection="$meta
+$script"
+
+# Escape newlines and backslashes for safe `sed` insertion
+escaped=$(echo "$injection" | sed 's/[&/\]/\\&/g' | sed ':a;N;$!ba;s/\n/\\n/g')
+
+sed -i "/<\/head>/i $escaped" "$file"
