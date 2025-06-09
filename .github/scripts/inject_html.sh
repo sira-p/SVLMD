57198251eaa72b266679700d76f8e34bec591515
Sira Pornsiriprasert <code@psira.me>
2025-06-09 22:38:53 +0700

    Auto saved by Logseq

--- .github/scripts/inject_html.sh
+++ .github/scripts/inject_html.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-meta=#!/bin/sh
-
 meta="<meta property=\"og:title\" content=\"Sira's Very Large Medical Database (SVLMD)\">
 <meta name=\"description\" content=\"Sira's Very Large Medical Database (SVLMD) is a community-maintained, expert-reviewed, non-hierarchical medical education database. The information prioritizes demographics, geography, and socioeconomy in the context of Thailand.\">
 <meta property=\"og:description\" content=\"Sira's Very Large Medical Database (SVLMD) is a community-maintained, expert-reviewed, non-hierarchical medical education database. The information prioritizes demographics, geography, and socioeconomy in the context of Thailand.\">
@@ -17,21 +15,6 @@ injection="$meta
 $script"
 
 # Escape newlines and backslashes for safe sed insertion
-escaped=$(echo "$injection" | sed 's/[&/\]/\\&/g' | sed ':a;N;$!ba;s/\n/\\n/g')
-
-sed -i "/<\/head>/i $escaped" "$file"
-
-
-script='<script>
-  document.title = document.title + " | SVLMD";
-</script>'
-
-file="$GITHUB_WORKSPACE/build/index.html"
-
-injection="$meta
-$script"
-
-# Escape newlines and backslashes for safe `sed` insertion
 escaped=$(echo "$injection" | sed 's/[&/\]/\\&/g' | sed ':a;N;$!ba;s/\n/\\n/g')
 
 sed -i "/<\/head>/i $escaped" "$file"
